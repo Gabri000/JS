f55691c4c88aa2cbeb1e1c912ef22c22ef946ec7
Gabriele <Gabri03@users.noreply.github.com>
2018-10-13 19:55:30 +0100

    Cao

--- assignements-server/js/script-02.js
+++ assignements-server/js/script-02.js
@@ -1,1 +1,2 @@
-document.getElementById("description-span").onclick = function() {window.open("assignement-02.htm#description", "_self")};
+var description-span = document.getElementById("description-span");
+description-span.onclick = function() {window.open("assignement-02.htm#description", "_self")};
