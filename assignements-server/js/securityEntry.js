c0c91674dc491d8bfec2c336b0ce0cd0b17f54ad
Gabriele <Gabri03@users.noreply.github.com>
2018-12-08 12:51:46 +0100

    Create securityEntry.js

---
+++ assignements-server/js/securityEntry.js
@@ -0,0 +1,14 @@
+
+  requestAccess();
+ function requestAccess(pass) {
+
+  var access = prompt("Please enter the password to acces the assignement:", "");
+  if (access == null || access == "") {
+    setTimeout(history.go(-1), 1);
+  } else {
+    if (access !== "passw0rd") {
+      setTimeout(history.go(-1), 1);
+    }
+      document.getElementById("container").style.display = "block";
+  }
+}
