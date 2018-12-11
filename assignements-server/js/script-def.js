652ce6ba10f192d4249c895811f55adf32e9b633
GG <36303258+Gabri000@users.noreply.github.com>
2018-12-11 15:17:51 +0100

    Create script-def.js

---
+++ assignements-server/js/script-def.js
@@ -0,0 +1,16 @@
+
+/*
+This page has been created by Gabriele BONANOMI
+*/
+
+function openPage(code, href) {
+
+  if (code = "i") {
+    window.open("../" + href + ".htm", "_self");
+  }
+
+  if (code = "a") {
+    window.open("assignement-" + href + ".htm", "_self");
+  }
+
+}
