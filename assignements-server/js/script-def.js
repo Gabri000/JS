ae65fdc86650b717f5fda2a70b0f33de67e91c50
GG <36303258+Gabri000@users.noreply.github.com>
2018-12-11 15:19:23 +0100

    Update script-def.js

--- assignements-server/js/script-def.js
+++ assignements-server/js/script-def.js
@@ -5,11 +5,11 @@ This page has been created by Gabriele BONANOMI
 
 function openPage(code, href) {
 
-  if (code = "i") {
+  if (code.includes("i")) {
     window.open("../" + href + ".htm", "_self");
   }
 
-  if (code = "a") {
+  if (code.includes("a")) {
     window.open("assignement-" + href + ".htm", "_self");
   }
 
