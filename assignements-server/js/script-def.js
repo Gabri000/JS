edd55acb8504006cc5f74f5ecd856e5c80f98f28
Gabriele <Gabri03@users.noreply.github.com>
2018-12-15 14:48:20 +0100

    Update script-def.js

--- assignements-server/js/script-def.js
+++ assignements-server/js/script-def.js
@@ -5,12 +5,18 @@ This page has been created by Gabriele BONANOMI
 
 function openPage(code, href) {
 
-  if (code.includes("i")) {
+  if (code.includes("indx")) {
     window.open("../" + href + ".htm", "_self");
   }
 
-  if (code.includes("a")) {
+  if (code.includes("asgn")) {
     window.open("assignement-" + href + ".htm", "_self");
+  }
+
+  if (code.includes("img")) {
+
+    window.open("../img/" + href + ".png", "_self");
+
   }
 
 }
